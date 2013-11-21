bc2376a00b9ec09677955f331aa2e7b46d4014ca
Xavier Calland <xca@atolcd.com>
2013-11-21 08:58:22 +0100

    Fix rsync deploy command
    Change-Id: Id6dc7d65c4fd30c566ad37a5d8666ee330c8d3c5

--- puppet/rsync_puppet.sh
+++ puppet/rsync_puppet.sh
@@ -6,5 +6,5 @@ MANIFEST_FILE='oasis-demo.pp'
 
 rsync -avz --delete --delete-excluded --exclude '.tmp' --exclude '.librarian' --exclude '.git' --exclude 'vagrant' --exclude '.gitignore' . -e ssh ${SSH_CONNECT}:${PUPPET_PATH}/
 
-ssh ${SSH_CONNECT} "puppet apply --modulepath '${PUPPET_PATH}/modules:${PUPPET_PATH}/project_modules/' --detailed-exitcodes ${PUPPET_PATH}/manifests/${MANIFEST_FILE}"
+ssh ${SSH_CONNECT} "puppet apply --modulepath '${PUPPET_PATH}/modules/deps/:${PUPPET_PATH}/modules/project/' --detailed-exitcodes ${PUPPET_PATH}/manifests/${MANIFEST_FILE}"
 
