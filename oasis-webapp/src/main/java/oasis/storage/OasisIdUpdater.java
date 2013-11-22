e3996a62dabef8b19541a75e9f1b952258c7c569
Bertrand Magnier <bma@atolcd.com>
2013-11-22 17:14:17 +0100

    Automatic UUID generation for Oasis objects
    Change-Id: I341f73ddf72a1162bfaf10088501150714b94c58

--- oasis-webapp/src/main/java/oasis/storage/OasisIdUpdater.java
+++ oasis-webapp/src/main/java/oasis/storage/OasisIdUpdater.java
@@ -3,12 +3,9 @@ package oasis.storage;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import org.bson.types.ObjectId;
 import org.jongo.ReflectiveObjectIdUpdater;
-
-import oasis.model.annotations.Id;
 
 public class OasisIdUpdater extends ReflectiveObjectIdUpdater {
 
@@ -21,51 +18,6 @@ public class OasisIdUpdater extends ReflectiveObjectIdUpdater {
   @Override
   public void setObjectId(Object target, ObjectId id) {
     super.setObjectId(target, id);
-    Field idField = findOasisIdField(target.getClass());
-    if (idField == null) {
-      return;
-    }
-
-    if (!isAnEmptyOasisId(target, idField) || !idField.getType().equals(String.class)) {
-      throw new IllegalArgumentException("Unable to set Oasis Id on class: " + target.getClass());
-    }
-    String uuid = UUID.randomUUID().toString();
-    updateOasisField(target, uuid, idField);
-  }
-
-  private boolean isAnEmptyOasisId(Object target, Field field) {
-    try {
-      field.setAccessible(true);
-      return field.get(target) == null;
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException("Unable to obtain value from field" + field.getName() + ", class: " + target.getClass(), e);
-    }
-  }
-
-  private void updateOasisField(Object target, String uuid, Field field) {
-    try {
-      if (field.getType().equals(String.class)) {
-        field.set(target, uuid);
-      }
-    } catch (IllegalAccessException e) {
-      throw new IllegalArgumentException("Unable to set Oasis Id on class: " + target.getClass());
-    }
-  }
-
-  private Field findOasisIdField(Class<?> clazz) {
-    if (oasisFieldCache.containsKey(clazz)) {
-      return oasisFieldCache.get(clazz);
-    }
-
-    while (!Object.class.equals(clazz)) {
-      for (Field f : clazz.getDeclaredFields()) {
-        if (f.isAnnotationPresent(Id.class)) {
-          oasisFieldCache.put(clazz, f);
-          return f;
-        }
-      }
-      clazz = clazz.getSuperclass();
-    }
-    return null;
+    OasisIdHelper.updateOasisIdField(target);
   }
 }
