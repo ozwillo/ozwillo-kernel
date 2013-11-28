8e8572df60b406a434761eb99af5d224f4437a52
Aurélien Ponçon <apo@atolcd.com>
2013-11-28 11:28:37 +0100

    Add authorization informations into access tokens.
    Change-Id: If78d8d7876447250a9de68e449a072080a880757

--- oasis-model/src/main/java/oasis/model/accounts/AccessToken.java
+++ oasis-model/src/main/java/oasis/model/accounts/AccessToken.java
@@ -1,9 +1,21 @@
 package oasis.model.accounts;
 
+import java.util.Set;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wordnik.swagger.annotations.ApiModelProperty;
 
 public class AccessToken extends Token {
   @JsonProperty
+  @ApiModelProperty
+  private String serviceProviderId;
+
+  @JsonProperty
+  @ApiModelProperty
+  private Set<String> scopeIds;
+
+  @JsonProperty
+  @ApiModelProperty
   private String refreshTokenId;
 
   public String getRefreshTokenId() {
@@ -12,5 +24,21 @@ public class AccessToken extends Token {
 
   public void setRefreshTokenId(String refreshTokenId) {
     this.refreshTokenId = refreshTokenId;
+  }
+
+  public String getServiceProviderId() {
+    return serviceProviderId;
+  }
+
+  public void setServiceProviderId(String serviceProviderId) {
+    this.serviceProviderId = serviceProviderId;
+  }
+
+  public Set<String> getScopeIds() {
+    return scopeIds;
+  }
+
+  public void setScopeIds(Set<String> scopeIds) {
+    this.scopeIds = scopeIds;
   }
 }
