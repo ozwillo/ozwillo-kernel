f54c565adea411e31a7bf9ae8d07c67318ff0289
Xavier Calland <xca@atolcd.com>
2013-12-02 10:24:54 +0100

    Subscription endpoint
    Change-Id: Ic92631f6f0446b87070516cfdc03168a018a14c4

--- oasis-webapp/src/main/java/oasis/web/eventbus/EventBusEndpoint.java
+++ oasis-webapp/src/main/java/oasis/web/eventbus/EventBusEndpoint.java
@@ -1,7 +1,6 @@
 package oasis.web.eventbus;
 
-import java.net.URI;
-
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
@@ -9,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wordnik.swagger.annotations.Api;
@@ -16,7 +16,9 @@ import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
 import com.wordnik.swagger.annotations.ApiOperation;
 
+import oasis.model.applications.ApplicationRepository;
 import oasis.model.applications.Subscription;
+import oasis.model.applications.SubscriptionRepository;
 import oasis.web.authn.Authenticated;
 import oasis.web.authn.Client;
 
@@ -25,6 +27,12 @@ import oasis.web.authn.Client;
 @Path("/e")
 @Api(value = "/e", description = "EventBus API")
 public class EventBusEndpoint {
+
+  @Inject
+  ApplicationRepository applications;
+
+  @Inject
+  SubscriptionRepository subscriptions;
 
   @POST
   @Path("/publish")
@@ -41,14 +49,27 @@ public class EventBusEndpoint {
   @Path("/{appId}/subscriptions")
   @Consumes(MediaType.APPLICATION_JSON)
   @ApiOperation(value = "Subscribe to a typed event from the event bus.",
-      notes = "The returned URL get access to the subscription (used to delete the subscription)",
-      response = URI.class)
+      notes = "The returned location URL get access to the subscription (delete the subscription)")
   public Response subscribe(
       @PathParam("appId") String appId,
       Subscription subscription
   ) {
-    //TODO: Code that make us dream
-    throw new UnsupportedOperationException();
+    // TODO: validate subscription.eventType
+
+    if (subscriptions.createSubscription(appId, subscription) == null) {
+      return Response
+          .status(Response.Status.NOT_FOUND)
+          .type(MediaType.TEXT_PLAIN_TYPE)
+          .entity("The application does not exist")
+          .build();
+    }
+
+    return Response
+        .created(UriBuilder
+            .fromResource(EventBusEndpoint.class)
+            .path(EventBusEndpoint.class, "unsubscribe")
+            .build(subscription.getId()))
+        .build();
   }
 
   @DELETE
@@ -58,11 +79,16 @@ public class EventBusEndpoint {
   public Response unsubscribe(
       @PathParam("subscriptionId") String subscriptionId
   ) {
-    //TODO: Code that don't make us cry
-    throw new UnsupportedOperationException();
-  }
+    if (!subscriptions.deleteSubscription(subscriptionId)) {
+      return Response
+          .status(Response.Status.NOT_FOUND)
+          .type(MediaType.TEXT_PLAIN_TYPE)
+          .entity("The subscription does not exist")
+          .build();
+    }
 
-  //TODO : Other functions here ?
+    return Response.noContent().build();
+  }
 
   @ApiModel
   static class Event {
