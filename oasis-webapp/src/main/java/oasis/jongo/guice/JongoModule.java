dc8750fcf23dba9648a3deb59760937077a88a90
Xavier Calland <xca@atolcd.com>
2013-12-06 08:28:53 +0100

    Move Jongo* bind from OasisGuiceModule to JongoModule
    Change-Id: Ia233e44291c7109d70a7341ba773e833ac49f024

--- oasis-webapp/src/main/java/oasis/jongo/guice/JongoModule.java
+++ oasis-webapp/src/main/java/oasis/jongo/guice/JongoModule.java
@@ -7,6 +7,24 @@ import com.mongodb.MongoClientURI;
 import com.typesafe.config.Config;
 
 import oasis.jongo.JongoService;
+import oasis.jongo.accounts.JongoAccountRepository;
+import oasis.jongo.applications.JongoApplicationRepository;
+import oasis.jongo.applications.JongoSubscriptionRepository;
+import oasis.jongo.authn.JongoTokenRepository;
+import oasis.jongo.authz.JongoAuthorizationRepository;
+import oasis.jongo.directory.JongoDirectoryRepository;
+import oasis.jongo.etag.JongoEtagService;
+import oasis.jongo.notification.JongoNotificationRepository;
+import oasis.jongo.social.JongoIdentityRepository;
+import oasis.model.accounts.AccountRepository;
+import oasis.model.applications.ApplicationRepository;
+import oasis.model.applications.SubscriptionRepository;
+import oasis.model.authn.TokenRepository;
+import oasis.model.authz.AuthorizationRepository;
+import oasis.model.directory.DirectoryRepository;
+import oasis.model.notification.NotificationRepository;
+import oasis.model.social.IdentityRepository;
+import oasis.services.etag.EtagService;
 
 public class JongoModule extends AbstractModule {
 
@@ -57,5 +75,15 @@ public class JongoModule extends AbstractModule {
   protected void configure() {
     bind(Settings.class).toInstance(settings);
     bind(Jongo.class).toProvider(JongoService.class);
+
+    bind(AccountRepository.class).to(JongoAccountRepository.class);
+    bind(IdentityRepository.class).to(JongoIdentityRepository.class);
+    bind(DirectoryRepository.class).to(JongoDirectoryRepository.class);
+    bind(NotificationRepository.class).to(JongoNotificationRepository.class);
+    bind(ApplicationRepository.class).to(JongoApplicationRepository.class);
+    bind(AuthorizationRepository.class).to(JongoAuthorizationRepository.class);
+    bind(TokenRepository.class).to(JongoTokenRepository.class);
+    bind(SubscriptionRepository.class).to(JongoSubscriptionRepository.class);
+    bind(EtagService.class).to(JongoEtagService.class);
   }
 }
--- oasis-webapp/src/main/java/oasis/web/guice/OasisGuiceModule.java
+++ oasis-webapp/src/main/java/oasis/web/guice/OasisGuiceModule.java
@@ -5,38 +5,9 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Clock;
 import com.google.inject.AbstractModule;
 
-import oasis.services.etag.EtagService;
-import oasis.jongo.etag.JongoEtagService;
-import oasis.model.accounts.AccountRepository;
-import oasis.model.applications.ApplicationRepository;
-import oasis.model.applications.SubscriptionRepository;
-import oasis.model.authn.TokenRepository;
-import oasis.model.authz.AuthorizationRepository;
-import oasis.model.directory.DirectoryRepository;
-import oasis.model.notification.NotificationRepository;
-import oasis.model.social.IdentityRepository;
-import oasis.jongo.accounts.JongoAccountRepository;
-import oasis.jongo.applications.JongoApplicationRepository;
-import oasis.jongo.applications.JongoSubscriptionRepository;
-import oasis.jongo.authn.JongoTokenRepository;
-import oasis.jongo.authz.JongoAuthorizationRepository;
-import oasis.jongo.directory.JongoDirectoryRepository;
-import oasis.jongo.notification.JongoNotificationRepository;
-import oasis.jongo.social.JongoIdentityRepository;
-
 public class OasisGuiceModule extends AbstractModule {
   @Override
   protected void configure() {
-    bind(AccountRepository.class).to(JongoAccountRepository.class);
-    bind(IdentityRepository.class).to(JongoIdentityRepository.class);
-    bind(DirectoryRepository.class).to(JongoDirectoryRepository.class);
-    bind(NotificationRepository.class).to(JongoNotificationRepository.class);
-    bind(ApplicationRepository.class).to(JongoApplicationRepository.class);
-    bind(AuthorizationRepository.class).to(JongoAuthorizationRepository.class);
-    bind(TokenRepository.class).to(JongoTokenRepository.class);
-    bind(SubscriptionRepository.class).to(JongoSubscriptionRepository.class);
-    bind(EtagService.class).to(JongoEtagService.class);
-
     bind(JsonFactory.class).to(JacksonFactory.class);
     bind(Clock.class).toInstance(Clock.SYSTEM);
   }
