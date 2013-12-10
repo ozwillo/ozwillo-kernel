66cddd173a5b39193fd9e1c6922cf929f9721b50
Aurélien Ponçon <apo@atolcd.com>
2013-12-10 09:18:29 +0100

    Validate the token received by the Introspection Endpoint.
    Change-Id: Ic57475e6a6e3788af019237f89f673aaddaaacc0

--- oasis-webapp/src/main/java/oasis/web/authz/IntrospectionEndpoint.java
+++ oasis-webapp/src/main/java/oasis/web/authz/IntrospectionEndpoint.java
@@ -1,26 +1,51 @@
 package oasis.web.authz;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
+import com.google.common.collect.Sets;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
+import oasis.model.accounts.AccessToken;
+import oasis.model.accounts.Account;
+import oasis.model.accounts.AccountRepository;
+import oasis.model.accounts.Token;
+import oasis.model.applications.ApplicationRepository;
+import oasis.model.applications.DataProvider;
+import oasis.model.authn.TokenRepository;
+import oasis.services.authn.TokenHandler;
+import oasis.services.authn.TokenSerializer;
 import oasis.web.authn.Authenticated;
 import oasis.web.authn.Client;
+import oasis.web.authn.ClientPrincipal;
 
 @Authenticated @Client
 @Path("/a/tokeninfo")
 @Api(value = "/a/tokeninfo", description = "Introspection Endpoint")
 public class IntrospectionEndpoint {
+  private static final Joiner SCOPE_JOINER = Joiner.on(' ').skipNulls();
+
+  @Context SecurityContext securityContext;
+  @Inject TokenRepository tokenRepository;
+  @Inject TokenHandler tokenHandler;
+  @Inject AccountRepository accountRepository;
+  @Inject ApplicationRepository applicationRepository;
 
   @POST
   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -31,14 +56,70 @@ public class IntrospectionEndpoint {
       response = IntrospectionResponse.class
   )
   public Response post(@FormParam("token") String token) throws IOException {
-    // TODO: Validate the token
+    DataProvider dataProvider = applicationRepository.getDataProvider(((ClientPrincipal) securityContext.getUserPrincipal()).getClientId());
+    if (dataProvider == null) {
+      return Response.status(Response.Status.UNAUTHORIZED)
+          .build();
+    }
+    if (Strings.isNullOrEmpty(token)) {
+      return error();
+    }
 
-    IntrospectionResponse introspectionResponse = new IntrospectionResponse()
-        .setActive(true);
+    Token unserializedToken = TokenSerializer.unserialize(token);
+    if (unserializedToken == null) {
+      return error();
+    }
+
+    // Preventive check in order to not make a call to the database
+    if (!tokenHandler.checkTokenValidity(unserializedToken)) {
+      return error();
+    }
+
+    Token unverifiedToken = tokenRepository.getToken(unserializedToken.getId());
+    if (!(unverifiedToken instanceof AccessToken)) {
+      return error();
+    }
+    // True check not based on the token given by the untrusted client
+    if (!tokenHandler.checkTokenValidity(unverifiedToken)) {
+      return error();
+    }
+
+    AccessToken accessToken = (AccessToken) unverifiedToken;
+
+    // XXX: load an entire account just for an account id ?
+    Account account = accountRepository.getAccountByTokenId(accessToken.getId());
+
+    IntrospectionResponse introspectionResponse;
+    long issuedAtTime = accessToken.getCreationTime().getMillis();
+    long expireAt = issuedAtTime + accessToken.getTimeToLive().getMillis();
+
+    Set<String> scopeIds = Sets.newHashSet(accessToken.getScopeIds());
+    // Remove all scopes which don't belong to the data provider
+    scopeIds.retainAll(dataProvider.getScopeIds());
+
+    if (scopeIds.isEmpty()) {
+      return error();
+    }
+
+    introspectionResponse = new IntrospectionResponse()
+        .setActive(true)
+        .setExp(TimeUnit.MILLISECONDS.toSeconds(expireAt))
+        .setIat(TimeUnit.MILLISECONDS.toSeconds(issuedAtTime))
+        .setScope(SCOPE_JOINER.join(scopeIds))
+        .setClient_id(accessToken.getServiceProviderId())
+        .setSub(account.getId())
+        .setToken_type("Bearer");
 
     return Response.ok()
         .header(HttpHeaders.CACHE_CONTROL, "no-store")
         .entity(introspectionResponse)
+        .build();
+  }
+
+  private Response error() {
+    return Response.ok()
+        .header(HttpHeaders.CACHE_CONTROL, "no-store")
+        .entity(new IntrospectionResponse().setActive(false))
         .build();
   }
 }
