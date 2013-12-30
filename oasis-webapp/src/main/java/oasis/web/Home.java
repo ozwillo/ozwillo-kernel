2ecf2f489a81dcc9cafa7586d287bb39db363979
Aurélien Ponçon <apo@atolcd.com>
2013-12-30 11:28:11 +0100

    Bootstrapify and Angularify the base template
    - Add JQuery 2, Bootstrap 3, Lodash and AngularJS
    - Add endpoints for CSS and JS files
    - Add a CSS and JS main files
    
    Change-Id: I2deee845da5370d1fb9c935e0abde7e61fc51dec

--- oasis-webapp/src/main/java/oasis/web/Home.java
+++ oasis-webapp/src/main/java/oasis/web/Home.java
@@ -1,10 +1,18 @@
 package oasis.web;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.google.common.io.Resources;
 
 import oasis.web.utils.ResponseFactory;
 import oasis.web.view.View;
@@ -23,5 +31,39 @@ public class Home {
   public Response favicon() {
     // TODO: we need a favicon!
     return ResponseFactory.NOT_FOUND;
+  }
+
+  @GET
+  @Path("{resource: .+\\.css}")
+  @Produces("text/css")
+  public Response css(@PathParam("resource") String resourceName) throws IOException {
+    return getResource(resourceName);
+  }
+
+  @GET
+  @Path("{resource: .+\\.js}")
+  @Produces("application/javascript")
+  public Response js(@PathParam("resource") String resourceName) throws IOException {
+    return getResource(resourceName);
+  }
+
+  private Response getResource(String resourceName) throws IOException {
+    final URL resource;
+    try {
+      resource = Resources.getResource("oasis-ui/" + resourceName);
+    } catch (IllegalArgumentException iae) {
+      return ResponseFactory.NOT_FOUND;
+    }
+
+    URLConnection conn = resource.openConnection();
+    Response.ResponseBuilder response = Response.ok()
+        .entity(conn.getInputStream());
+
+    long lastModified = conn.getLastModified();
+    if (lastModified != 0) {
+      response.lastModified(new Date(lastModified));
+    }
+
+    return response.build();
   }
 }
