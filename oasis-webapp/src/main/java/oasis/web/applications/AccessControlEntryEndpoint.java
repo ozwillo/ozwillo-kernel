04ac327d7bcafd595416e85a2fef42adedf808ea
Thomas Broyer <t.broyer@ltgt.net>
2014-09-11 12:21:32 +0200

    Control access in AccessControlEntryEndpoint (sic!)
    Only app_admins can change ACL for app-instances.
    
    Change-Id: I9eafcadec55e934cfd66fa15428e345b936df0a6

--- oasis-webapp/src/main/java/oasis/web/applications/AccessControlEntryEndpoint.java
+++ oasis-webapp/src/main/java/oasis/web/applications/AccessControlEntryEndpoint.java
@@ -8,9 +8,11 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 import com.google.common.base.Strings;
 import com.wordnik.swagger.annotations.Api;
@@ -19,9 +21,12 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import oasis.model.InvalidVersionException;
 import oasis.model.applications.v2.AccessControlEntry;
 import oasis.model.applications.v2.AccessControlRepository;
+import oasis.model.applications.v2.AppInstanceRepository;
+import oasis.services.authz.AppAdminHelper;
 import oasis.services.etag.EtagService;
 import oasis.web.authn.Authenticated;
 import oasis.web.authn.OAuth;
+import oasis.web.authn.OAuthPrincipal;
 import oasis.web.utils.ResponseFactory;
 
 @Path("/apps/acl/ace/{ace_id}")
@@ -31,7 +36,11 @@ import oasis.web.utils.ResponseFactory;
 @Api(value = "acl-ace", description = "Access Control Entry")
 public class AccessControlEntryEndpoint {
   @Inject AccessControlRepository accessControlRepository;
+  @Inject AppInstanceRepository appInstanceRepository;
+  @Inject AppAdminHelper appAdminHelper;
   @Inject EtagService etagService;
+
+  @Context SecurityContext securityContext;
 
   @PathParam("ace_id") String ace_id;
 
@@ -44,6 +53,9 @@ public class AccessControlEntryEndpoint {
     AccessControlEntry ace = accessControlRepository.getAccessControlEntry(ace_id);
     if (ace == null) {
       return ResponseFactory.NOT_FOUND;
+    }
+    if (!isAppAdmin(((OAuthPrincipal) securityContext.getUserPrincipal()).getAccessToken().getAccountId(), ace.getInstance_id())) {
+      return ResponseFactory.forbidden("Current user is not an app_admin for the application instance");
     }
     return Response.ok()
         .tag(etagService.getEtag(ace))
@@ -58,7 +70,13 @@ public class AccessControlEntryEndpoint {
       return ResponseFactory.preconditionRequiredIfMatch();
     }
 
-    // TODO: only admins can delete ACEs
+    AccessControlEntry ace = accessControlRepository.getAccessControlEntry(ace_id);
+    if (ace == null) {
+      return ResponseFactory.NOT_FOUND;
+    }
+    if (!isAppAdmin(((OAuthPrincipal) securityContext.getUserPrincipal()).getAccessToken().getAccountId(), ace.getInstance_id())) {
+      return ResponseFactory.forbidden("Current user is not an app_admin for the application instance");
+    }
 
     boolean deleted;
     try {
@@ -72,5 +90,9 @@ public class AccessControlEntryEndpoint {
     }
 
     return ResponseFactory.NO_CONTENT;
+  }
+
+  private boolean isAppAdmin(String userId, String instanceId) {
+    return appAdminHelper.isAdmin(userId, appInstanceRepository.getAppInstance(instanceId));
   }
 }
