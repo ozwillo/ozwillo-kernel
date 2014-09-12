0f073b7dd71a4d9e751583dc2c5944adba4b1152
Thomas Broyer <t.broyer@ltgt.net>
2014-09-12 15:37:01 +0200

    Correctly determine app_admin for apps bought by individuals
    Change-Id: I3a4015e418a99d4a4b795d40482edc6593761ac0

--- oasis-webapp/src/main/java/oasis/services/authz/AppAdminHelper.java
+++ oasis-webapp/src/main/java/oasis/services/authz/AppAdminHelper.java
@@ -2,6 +2,8 @@ package oasis.services.authz;
 
 import javax.inject.Inject;
 
+import com.google.common.base.Strings;
+
 import oasis.model.applications.v2.AppInstance;
 import oasis.model.directory.OrganizationMembership;
 import oasis.model.directory.OrganizationMembershipRepository;
@@ -16,7 +18,13 @@ public class AppAdminHelper {
 
   public boolean isAdmin(String userId, AppInstance instance) {
     // TODO: support applications bought by individuals
-    OrganizationMembership membership = organizationMembershipRepository.getOrganizationMembership(userId, instance.getProvider_id());
-    return membership != null && membership.isAdmin();
+    if (Strings.isNullOrEmpty(instance.getProvider_id())) {
+      // Application bought by an individual
+      return userId.equals(instance.getInstantiator_id());
+    } else {
+      // Application bought by/for an organization
+      OrganizationMembership membership = organizationMembershipRepository.getOrganizationMembership(userId, instance.getProvider_id());
+      return membership != null && membership.isAdmin();
+    }
   }
 }
