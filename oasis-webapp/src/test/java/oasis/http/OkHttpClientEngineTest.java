47222bb1955b2ccfe84025dd2d61a4b067e0efa0
Thomas Broyer <t.broyer@ltgt.net>
2015-04-14 11:32:31 +0200

    Enhance OkHttpClientEngineTest
    Change-Id: I75760b6c9195db55c422e3dd3fec57c194105c1b

--- oasis-webapp/src/test/java/oasis/http/OkHttpClientEngineTest.java
+++ oasis-webapp/src/test/java/oasis/http/OkHttpClientEngineTest.java
@@ -16,9 +16,9 @@ import org.jukito.JukitoRunner;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 
-import com.google.api.client.repackaged.com.google.common.base.Strings;
 import com.squareup.okhttp.mockwebserver.Dispatcher;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.RecordedRequest;
@@ -40,16 +40,30 @@ public class OkHttpClientEngineTest {
     }
   }
 
+  // In case an error is thrown in the MockWebServer, so clients don't block infinitely.
+  @Rule public Timeout timeout = Timeout.seconds(10);
+
   @Rule public MockWebServerRule mockServer = new MockWebServerRule();
   @Before public void setUp() {
     mockServer.get().setDispatcher(new Dispatcher() {
       @Override
       public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
-        assertThat(request.getBody().readByteArray()).isEqualTo(PAYLOAD);
-        return new MockResponse()
-            .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN)
-            .setHeader("X-Whatever", "some header")
-            .setBody(Strings.nullToEmpty(request.getHeader(WebhookSignatureFilter.HEADER)));
+        switch (request.getPath()) {
+          case "/simple":
+            return new MockResponse()
+                .setResponseCode(Response.Status.NOT_FOUND.getStatusCode())
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN)
+                .setHeader("X-Whatever", "some header")
+                .setBody("Not found");
+          case "/writerInterceptor":
+            assertThat(request.getBody().readByteArray()).isEqualTo(PAYLOAD);
+            // fall-through
+          case "/requestHeader":
+            return new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN)
+                .setBody(request.getHeader(WebhookSignatureFilter.HEADER));
+        }
+        throw new AssertionError("Unexpected request: " + request);
       }
     });
   }
@@ -57,18 +71,18 @@ public class OkHttpClientEngineTest {
   @Inject Client client;
 
   @Test public void simpleRequest() {
-    Response response = client.target(mockServer.getUrl("/simple").toString()).request()
-        .post(Entity.json(PAYLOAD));
+    Response response = client.target(mockServer.getUrl("/simple").toString()).request().get();
 
-    assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
+    assertThat(response.getStatusInfo()).isEqualTo(Response.Status.NOT_FOUND);
     assertThat(response.getMediaType()).isEqualTo(MediaType.TEXT_PLAIN_TYPE);
     assertThat(response.getHeaderString("X-Whatever")).isEqualTo("some header");
+    assertThat(response.readEntity(String.class)).isEqualTo("Not found");
   }
 
   @Test public void requestHeader() {
     Response response = client.target(mockServer.getUrl("/requestHeader").toString()).request()
         .header(WebhookSignatureFilter.HEADER, SIGNATURE)
-        .post(Entity.json(PAYLOAD));
+        .get();
 
     assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
     String signature = response.readEntity(String.class);
