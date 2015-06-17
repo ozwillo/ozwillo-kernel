ozwillo/ozwillo-kernel

commit 1e208ac4c2e0451fe88d199a610f075b6422d89b

Allow resetting (some) user account/profile fields to null/empty
TODO: find a better way to do it, leveraging Jackson, and apply it
to all updates, everywhere. Solving the LocalizableValue issue where
you remove a translation will be hard though, as we cannot just '$unset'
"unknown" fields (should we then read the current value to make a diff
and hope for the best wrt concurrent updates?)

Change-Id: I697d32f468de86e029e3b84a05cbdde5790246a7

oasis-webapp/src/main/java/oasis/jongo/accounts/JongoAccountRepository.java
@@ -17,6 +17,8 @@
  */
 package oasis.jongo.accounts;
 
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.jongo.Jongo;
@@ -26,12 +28,14 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 import com.google.common.primitives.Longs;
 import com.mongodb.DuplicateKeyException;
 
 import oasis.jongo.JongoBootstrapper;
 import oasis.model.InvalidVersionException;
 import oasis.model.accounts.AccountRepository;
+import oasis.model.accounts.Address;
 import oasis.model.accounts.UserAccount;
 
 public class JongoAccountRepository implements AccountRepository, JongoBootstrapper {
@@ -88,9 +92,59 @@ public class JongoAccountRepository implements AccountRepository, JongoBootstrap
     account.setEmail_verified(null);
     account.setPhone_number_verified(null);
     account.setCreated_at(null);
+    // Allow resetting fields to null/empty
+    // TODO: find a better way to do it! (leveraging Jackson)
+    Map<String, Boolean> unsetObject = Maps.newLinkedHashMap();
+    // NOTE: don't allow resetting the nickname and locale (set during account creation), and zoneinfo
+    if (account.getName() == null) {
+      unsetObject.put("name", true);
+    }
+    if (account.getFamily_name() == null) {
+      unsetObject.put("family_name", true);
+    }
+    if (account.getMiddle_name() == null) {
+      unsetObject.put("middle_name", true);
+    }
+    if (account.getGiven_name() == null) {
+      unsetObject.put("given_name", true);
+    }
+    if (account.getPicture() == null) {
+      unsetObject.put("picture", true);
+    }
+    if (account.getBirthdate() == null) {
+      unsetObject.put("birthdate", true);
+    }
+    if (account.getGender() == null) {
+      unsetObject.put("gender", true);
+    }
+    if (account.getPhone_number() == null) {
+      unsetObject.put("phone_number", true);
+    }
+    if (account.getAddress() == null) {
+      unsetObject.put("address", true);
+    } else {
+      // individual address fields
+      Address address = account.getAddress();
+      if (address.getStreet_address() == null) {
+        unsetObject.put("address.street_address", true);
+      }
+      if (address.getPostal_code() == null) {
+        unsetObject.put("address.postal_code", true);
+      }
+      if (address.getLocality() == null) {
+        unsetObject.put("address.locality", true);
+      }
+      if (address.getRegion() == null) {
+        unsetObject.put("address.region", true);
+      }
+      if (address.getCountry() == null) {
+        unsetObject.put("address.country", true);
+      }
+    }
+
     account = getAccountCollection()
         .findAndModify("{ id: #, updated_at: { $in: # } }", id, Longs.asList(versions))
-        .with("{ $set: # }", account)
+        .with("{ $set: #, $unset: # }", account, unsetObject)
         .returnNew()
         .as(JongoUserAccount.class);
     if (account == null) {

commit aeadd0b1127a959e87ea8681a2f62fa019543e42

Fix regression introduced in 1e208ac4c2e0451fe88d199a610f075b6422d89b
Jongo/Jackson serializes an account's address as {'address':{…}},
so unsetting address.street_address (for instance) causes a Mongo
error:

    "exception: Cannot update 'address' and 'address.street_address' at the same time"

Change-Id: I4e37a06eb8aa8e99b4181a369d08ef656a7d1435

oasis-webapp/src/main/java/oasis/jongo/accounts/JongoAccountRepository.java
@@ -122,24 +122,6 @@ public class JongoAccountRepository implements AccountRepository, JongoBootstrap
     }
     if (account.getAddress() == null) {
       unsetObject.put("address", true);
-    } else {
-      // individual address fields
-      Address address = account.getAddress();
-      if (address.getStreet_address() == null) {
-        unsetObject.put("address.street_address", true);
-      }
-      if (address.getPostal_code() == null) {
-        unsetObject.put("address.postal_code", true);
-      }
-      if (address.getLocality() == null) {
-        unsetObject.put("address.locality", true);
-      }
-      if (address.getRegion() == null) {
-        unsetObject.put("address.region", true);
-      }
-      if (address.getCountry() == null) {
-        unsetObject.put("address.country", true);
-      }
     }
 
     account = getAccountCollection()