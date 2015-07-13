ozwillo/ozwillo-kernel

commit 9c7217111e39a7378ee8879dbca60d57220359d4

OAuth codes/tokens are children of the session (except for offline_access)
Note: we store the authorization_code ID as parent of the tokens,
although it's been revoked, to be able to detect code reuse, and
revoke all previously issued tokens based on the reused code.
(see http://tools.ietf.org/html/rfc6749#section-4.1.2)
We do not yet detect code reuse though.

Change-Id: I1fcc54442af687f68eecb5af692a40543d32b8cd

oasis-webapp/src/main/java/oasis/services/authn/TokenHandler.java
@@ -58,6 +58,10 @@ public class TokenHandler {
     accessToken.expiresIn(oidcSettings.accessTokenDuration);
     accessToken.setScopeIds(authorizationCode.getScopeIds());
     accessToken.setServiceProviderId(authorizationCode.getServiceProviderId());
+    // Note: store the authorizationCode ID although it's been revoked to be able to
+    // detect code reuse, and revoke all previously issued tokens based on the reused code.
+    // (see http://tools.ietf.org/html/rfc6749#section-4.1.2)
+    accessToken.setParent(authorizationCode);
 
     secureToken(accessToken, pass);
 
@@ -87,12 +91,10 @@ public class TokenHandler {
     return accessToken;
   }
 
-  public AuthorizationCode createAuthorizationCode(String accountId, Set<String> scopeIds, String serviceProviderId,
+  public AuthorizationCode createAuthorizationCode(SidToken sidToken, Set<String> scopeIds, String serviceProviderId,
       @Nullable String nonce, String redirectUri, boolean shouldSendAuthTime, String pass) {
-    checkArgument(!Strings.isNullOrEmpty(accountId));
-
     AuthorizationCode authorizationCode = new AuthorizationCode();
-    authorizationCode.setAccountId(accountId);
+    authorizationCode.setAccountId(sidToken.getAccountId());
     // A AuthorizationCode is available only for 1 minute
     authorizationCode.expiresIn(Duration.standardMinutes(1));
     authorizationCode.setScopeIds(scopeIds);
@@ -100,11 +102,14 @@ public class TokenHandler {
     authorizationCode.setNonce(nonce);
     authorizationCode.setRedirectUri(redirectUri);
     authorizationCode.setShouldSendAuthTime(shouldSendAuthTime);
+    if (!scopeIds.contains("offline_access")) {
+      authorizationCode.setParent(sidToken);
+    }
 
     secureToken(authorizationCode, pass);
 
     // Register the new token
-    if (!tokenRepository.registerToken(accountId, authorizationCode)) {
+    if (!tokenRepository.registerToken(sidToken.getAccountId(), authorizationCode)) {
       return null;
     }
 
@@ -123,6 +128,10 @@ public class TokenHandler {
     refreshToken.expiresIn(Duration.standardDays(50 * 365));
     refreshToken.setScopeIds(authorizationCode.getScopeIds());
     refreshToken.setServiceProviderId(authorizationCode.getServiceProviderId());
+    // Note: store the authorizationCode ID although it's been revoked to be able to
+    // detect code reuse, and revoke all previously issued tokens based on the reused code.
+    // (see http://tools.ietf.org/html/rfc6749#section-4.1.2)
+    refreshToken.setParent(authorizationCode);
 
     secureToken(refreshToken, pass);
 

oasis-webapp/src/main/java/oasis/web/authz/AuthorizationEndpoint.java
@@ -196,7 +196,7 @@ public class AuthorizationEndpoint {
     Set<String> authorizedScopeIds = getAuthorizedScopeIds(serviceProvider.getId(), sidToken.getAccountId());
     if (authorizedScopeIds.containsAll(scopeIds) && !prompt.consent) {
       // User already authorized all claimed scopes, let it be a "transparent" redirect
-      return generateAuthorizationCodeAndRedirect(sidToken.getAccountId(), scopeIds, serviceProvider.getId(), nonce, redirect_uri, shouldSendAuthTime);
+      return generateAuthorizationCodeAndRedirect(sidToken, scopeIds, serviceProvider.getId(), nonce, redirect_uri, shouldSendAuthTime);
     }
 
     if (!prompt.interactive) {
@@ -223,11 +223,11 @@ public class AuthorizationEndpoint {
     initRedirectUriBuilder(redirect_uri);
     appendQueryParam("state", state);
 
-    String userId = ((UserSessionPrincipal) securityContext.getUserPrincipal()).getSidToken().getAccountId();
+    SidToken sidToken = ((UserSessionPrincipal) securityContext.getUserPrincipal()).getSidToken();
 
-    authorizationRepository.authorize(userId, client_id, selectedScopeIds);
+    authorizationRepository.authorize(sidToken.getAccountId(), client_id, selectedScopeIds);
 
-    return generateAuthorizationCodeAndRedirect(userId, scopeIds, client_id, nonce, redirect_uri, shouldSendAuthTime);
+    return generateAuthorizationCodeAndRedirect(sidToken, scopeIds, client_id, nonce, redirect_uri, shouldSendAuthTime);
   }
 
   private Response redirectToLogin(UriInfo uriInfo, Prompt prompt) {
@@ -246,10 +246,10 @@ public class AuthorizationEndpoint {
     return UserAuthenticationFilter.loginResponse(continueUrl.build(), redirectUriBuilder.toString(), securityContext);
   }
 
-  private Response generateAuthorizationCodeAndRedirect(String userId, Set<String> scopeIds, String client_id,
+  private Response generateAuthorizationCodeAndRedirect(SidToken sidToken, Set<String> scopeIds, String client_id,
       @Nullable String nonce, String redirect_uri, boolean shouldSendAuthTime) {
     String pass = tokenHandler.generateRandom();
-    AuthorizationCode authCode = tokenHandler.createAuthorizationCode(userId, scopeIds, client_id, nonce, redirect_uri, shouldSendAuthTime, pass);
+    AuthorizationCode authCode = tokenHandler.createAuthorizationCode(sidToken, scopeIds, client_id, nonce, redirect_uri, shouldSendAuthTime, pass);
 
     String auth_code = TokenSerializer.serialize(authCode, pass);
 

oasis-webapp/src/test/java/oasis/web/authz/AuthorizationEndpointTest.java
@@ -154,7 +154,7 @@ public class AuthorizationEndpointTest {
         }});
 
     when(tokenHandler.generateRandom()).thenReturn("pass");
-    when(tokenHandler.createAuthorizationCode(eq(sidToken.getAccountId()), anySetOf(String.class), eq(serviceProvider.getId()),
+    when(tokenHandler.createAuthorizationCode(eq(sidToken), anySetOf(String.class), eq(serviceProvider.getId()),
         anyString(), eq("https://application/callback"), anyBoolean(), anyString())).thenReturn(authorizationCode);
   }
 
@@ -188,7 +188,7 @@ public class AuthorizationEndpointTest {
 
     assertRedirectToApplication(response);
 
-    verify(tokenHandler).createAuthorizationCode(sidToken.getAccountId(), ImmutableSet.of(openidScope.getId()), serviceProvider.getId(), null,
+    verify(tokenHandler).createAuthorizationCode(sidToken, ImmutableSet.of(openidScope.getId()), serviceProvider.getId(), null,
         "https://application/callback", false, "pass");
   }
 
@@ -454,7 +454,7 @@ public class AuthorizationEndpointTest {
 
     assertRedirectToApplication(response);
 
-    verify(tokenHandler).createAuthorizationCode(sidToken.getAccountId(), ImmutableSet.of(openidScope.getId()), serviceProvider.getId(), null,
+    verify(tokenHandler).createAuthorizationCode(sidToken, ImmutableSet.of(openidScope.getId()), serviceProvider.getId(), null,
         "https://application/callback", false, "pass");
   }
 
@@ -621,7 +621,7 @@ public class AuthorizationEndpointTest {
 
     assertRedirectToApplication(response);
 
-    verify(tokenHandler).createAuthorizationCode(sidToken.getAccountId(), ImmutableSet.of(openidScope.getId()), serviceProvider.getId(), null,
+    verify(tokenHandler).createAuthorizationCode(sidToken, ImmutableSet.of(openidScope.getId()), serviceProvider.getId(), null,
         "https://application/callback", true, "pass");
   }
 

commit d8141cb36cc272b2308a22267a822203860ac337

Fix regression in (offline) refresh tokens
This was introduced in 9c7217111e39a7378ee8879dbca60d57220359d4 and then
made worse by e2cb2a822961abc6bcc578b6efb144d79f246cf1 that made it
unconditional.

Even with offline_access, the authorization code is a "child" of the
session (that way, we can revoke it when revoking the session, and get
the session's auth_time), but the refresh token is not (it's still a
"child" of the authorization code though, so we can eventually revoke
it in case of code reuse).

Change-Id: If3e321cc5a6340ee05da9f2c8e980e20a4be856d

oasis-webapp/src/main/java/oasis/services/authn/TokenHandler.java
@@ -28,6 +28,7 @@ import javax.inject.Inject;
 
 import com.google.api.client.util.Clock;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
 import com.google.common.io.BaseEncoding;
 
 import oasis.model.authn.AccessToken;
@@ -40,7 +41,6 @@ import oasis.model.authn.SidToken;
 import oasis.model.authn.Token;
 import oasis.model.authn.TokenRepository;
 import oasis.auth.AuthModule;
-import oasis.model.authz.Scopes;
 import oasis.services.authn.login.PasswordHasher;
 import oasis.userdirectory.UserDirectoryModule;
 
@@ -155,9 +155,7 @@ public class TokenHandler {
     authorizationCode.setServiceProviderId(serviceProviderId);
     authorizationCode.setNonce(nonce);
     authorizationCode.setRedirectUri(redirectUri);
-    if (!scopeIds.contains(Scopes.OFFLINE_ACCESS)) {
-      authorizationCode.setParent(sidToken);
-    }
+    authorizationCode.setParent(sidToken);
 
     secureToken(authorizationCode, pass);
 
@@ -183,7 +181,7 @@ public class TokenHandler {
     // Note: store the authorizationCode ID although it's been revoked to be able to
     // detect code reuse, and revoke all previously issued tokens based on the reused code.
     // (see http://tools.ietf.org/html/rfc6749#section-4.1.2)
-    refreshToken.setParent(authorizationCode);
+    refreshToken.setAncestorIds(ImmutableList.of(authorizationCode.getId()));
 
     secureToken(refreshToken, pass);
 

oasis-webapp/src/test/java/oasis/web/authz/TokenEndpointTest.java
@@ -48,6 +48,7 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.testing.http.FixedClock;
 import com.google.api.client.util.Clock;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
@@ -72,7 +73,6 @@ import oasis.services.authn.TokenHandler;
 import oasis.services.authn.TokenSerializer;
 import oasis.services.authz.AppAdminHelper;
 import oasis.urls.ImmutableUrls;
-import oasis.urls.Urls;
 import oasis.urls.UrlsModule;
 import oasis.web.authn.testing.TestClientAuthenticationFilter;
 
@@ -160,7 +160,7 @@ public class TokenEndpointTest {
     setAccountId(account.getId());
     setCreationTime(now);
     expiresIn(Duration.standardDays(100));
-    setParent(validAuthCodeWithOfflineAccess);
+    setAncestorIds(ImmutableList.of(validAuthCodeWithOfflineAccess.getId()));
     setServiceProviderId(validAuthCodeWithOfflineAccess.getServiceProviderId());
     setScopeIds(validAuthCodeWithOfflineAccess.getScopeIds());
   }};